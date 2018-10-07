59c52acb1ed4096b0563886420381f7e377d2375
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2018-10-07 20:56:25 +1100

    base version

---
+++ src/main/java/com/aravindan/petclinic/AravindanPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.aravindan.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AravindanPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AravindanPetClinicApplication.class, args);
+	}
+}
